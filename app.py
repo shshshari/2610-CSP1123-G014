3b13521f05fce4a8229038272e5cc057446e908e
Sharizat <spillittapioca@gmail.com>
2026-04-20 18:30:19 +0800

    fix: line 2 wrong file name

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from recc_routes import recc_bp
+from reccsystem import recc_bp
 
 app = Flask(__name__)
-app.secret_key = "placeholder_secret_key" 
-
+app.secret_key = "placeholder_secret_key"
 
 app.register_blueprint(recc_bp)
 
